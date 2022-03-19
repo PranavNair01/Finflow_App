4d363c0880e965c8edfb57a231cd769792ec5d31
PranavNair01 <n.pranav@iitg.ac.in>
2022-03-19 12:45:02 +0530

    Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:finflow/screens/home_page.dart';
 import 'package:flutter/material.dart';
 import 'screens/signup_page.dart';
 import 'screens/otp_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
